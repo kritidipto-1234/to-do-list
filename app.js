cd0d8dc0642e9a5403da713a33211686284c72f4
Kritidipto Ghosh <kritidiptodgp.ghosh@gmail.com>
2020-12-09 13:05:41 +0530

    adding files

---
+++ app.js
@@ -0,0 +1,105 @@
+const add=document.querySelector('.submit');
+document.querySelector('.close').style.visibility="hidden";
+let message=document.querySelector('.message');
+let searchBTN=document.querySelector("input[value='Search']");
+message.style.display="none";
+let filterTask=document.querySelector("input[name='filterText']");
+let filterDuration=document.querySelector("input[name='filterDuration']");
+let refreshBTN=document.querySelector('.refresh');
+
+function removetask(element)
+{
+    element.srcElement.parentElement.remove();
+    let message=document.querySelector('.message');
+    message.textContent="Task Removed!";
+    message.style.backgroundColor='#F04124';
+    message.style.display="flex";
+    setTimeout(function(){message.style.display="none";}, 3000);
+}
+
+function addtask()
+{
+    message.style.display="flex";
+    setTimeout(function(){message.style.display="none";}, 3000);
+
+    let taskvalue=document.querySelector("input[name='task']").value;
+    let deadlinevalue=document.querySelector("input[name='deadline']").value;
+    let durationvalue=document.querySelector("input[name='duration']").value;
+    
+    if (!taskvalue || !deadlinevalue || !durationvalue)
+    {
+        message.textContent="Please fill in all fields!";
+        message.style.backgroundColor='#F04124';
+        return;
+    }
+    
+    let newTask=document.createElement('div');
+    newTask.className='item';
+    
+    let task=document.createElement('div');
+    task.classList.add('info');
+    task.textContent=taskvalue;
+    newTask.appendChild(task);
+    
+    let deadline=document.createElement('div');
+    deadline.classList.add('info');
+    deadline.textContent=deadlinevalue;
+    newTask.appendChild(deadline);
+    
+    let duration=document.createElement('div');
+    duration.classList.add('info');
+    duration.textContent=durationvalue;
+    newTask.appendChild(duration);
+    
+    let close=document.createElement('div');
+    close.className='close';
+    newTask.appendChild(close);
+    
+    let list=document.querySelector('.list');
+    list.appendChild(newTask);
+    
+    message.textContent='Successfully added new task to list !'
+    message.style.backgroundColor='Green';
+
+    document.querySelectorAll('.close').forEach(item=> item.addEventListener('click',removetask));
+}
+
+document.querySelectorAll('.close').forEach(item=> item.addEventListener('click',removetask));
+
+
+
+function search()
+{
+    if (!filterDuration.value && !filterTask.value)
+    {
+        message.style.display="flex";
+        message.style.backgroundColor='#F04124';
+        message.textContent='Fill in atleast one field for searching!';
+        setTimeout(function(){message.style.display="none";}, 3000);
+        return;
+    }
+    if(filterDuration.value)
+    {
+        let [,...list]=document.querySelectorAll('.info:nth-child(3)');
+        list.forEach(item=>{if (Number(item.textContent)>Number(filterDuration.value)) item.parentElement.style.display="none";})
+    }
+
+    if(filterTask.value)
+    {
+        let [,...list]=document.querySelectorAll('.info:nth-child(1)');
+        let r=new RegExp(filterTask.value,"i");
+        list.forEach(item=>{if (!item.textContent.match(r,"i")) item.parentElement.style.display="none";})
+    }
+}
+
+function refresh()
+{
+    filterDuration.value='';
+    filterTask.value='';
+    let list=document.querySelectorAll('.item');
+    list.forEach(item=>item.style.display='flex');
+}
+
+add.addEventListener('click',addtask);
+searchBTN.addEventListener('click',search);
+refreshBTN.addEventListener('click',refresh);
